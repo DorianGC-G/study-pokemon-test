432e1ba7240d602189c5cf9f430b5cbfb7dc36f2
DorianGC-G <dorian.guitel@gmail.com>
2022-05-18 13:43:48 +0200

    added random seed to test the models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+p "resetting"
+
+User.destroy_all
+Quizz.destroy_all
+
+p "creating users"
+
+user1 = User.create(email: "dorian@example.com", password: "123456")
+user1.save
+
+p "creating quizzes"
+
+quizz1 = Quizz.create(
+  title: "Hello there"
+)
+
+questions = Question.create([
+  {content: "Who said Hello There ?", quizz: quizz1, answers: Answer.create([
+    {content: "General Kenobi", correct_answer: true},
+    {content: "General Grievous", correct_answer: false},
+    {content: "Darth Vador", correct_answer: false},
+    {content: "Rick Astley", correct_answer: false}])
+  },
+  {content: "Who said Hello There ?", quizz: quizz1, answers: Answer.create([
+    {content: "General Kenobi", correct_answer: true},
+    {content: "General Grievous", correct_answer: false},
+    {content: "Darth Vador", correct_answer: false},
+    {content: "Rick Astley", correct_answer: false}])
+  },
+  {content: "Who said Hello There ?", quizz: quizz1, answers: Answer.create([
+    {content: "General Kenobi", correct_answer: true},
+    {content: "General Grievous", correct_answer: false},
+    {content: "Darth Vador", correct_answer: false},
+    {content: "Rick Astley", correct_answer: false}])
+  },
+  {content: "Who said Hello There ?", quizz: quizz1, answers: Answer.create([
+    {content: "General Kenobi", correct_answer: true},
+    {content: "General Grievous", correct_answer: false},
+    {content: "Darth Vador", correct_answer: false},
+    {content: "Rick Astley", correct_answer: false}])
+  },
+])
+
+quizz1.save
+
+p "done"
