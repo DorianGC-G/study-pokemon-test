8f64c8a35dcd5e3b569af002b641e857a140e416
DorianGC-G <dorian.guitel@gmail.com>
2022-05-19 16:53:58 +0200

    adjusted the seed to comply with last tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,140 +39,137 @@ eevee = {
 
 p "creating quizzes"
 quizz1 = Quizz.create(
-  title: "Guess the pokemon's name"
+  title: "Guess the pokemon's name",
+  questions: Question.create([
+    {content: bulbasaur[:description], quizz: quizz1, answers: Answer.create([
+      {content: "Bulbasaur", correct_answer: true},
+      {content: "Charmander", correct_answer: false},
+      {content: "Pikachu", correct_answer: false},
+      {content: "Rick Astley", correct_answer: false}])
+    },
+    {content: pikachu[:description], quizz: quizz1, answers: Answer.create([
+      {content: "General Kenobi", correct_answer: false},
+      {content: "Bulbasaur", correct_answer: false},
+      {content: "Charmander", correct_answer: false},
+      {content: "Pikachu", correct_answer: true}])
+    },
+    {content: eevee[:description], quizz: quizz1, answers: Answer.create([
+      {content: "General Grievous", correct_answer: false},
+      {content: "Eevee", correct_answer: true},
+      {content: "Pikachu", correct_answer: false},
+      {content: "Squirtle", correct_answer: false}])
+    },
+    {content: squirtle[:description], quizz: quizz1, answers: Answer.create([
+      {content: "Charizard", correct_answer: false},
+      {content: "Charmander", correct_answer: false},
+      {content: "Squirtle", correct_answer: true},
+      {content: "Psykokwak", correct_answer: false}])
+    },
+    {content: charmander[:description], quizz: quizz1, answers: Answer.create([
+      {content: "Charizard", correct_answer: false},
+      {content: "Charmander", correct_answer: true},
+      {content: "Agumon", correct_answer: false},
+      {content: "Muk", correct_answer: false}])
+    },
+  ])
 )
 
-questions_quizz_1 = Question.create([
-  {content: bulbasaur[:description], quizz: quizz1, answers: Answer.create([
-    {content: "Bulbasaur", correct_answer: true},
-    {content: "Charmander", correct_answer: false},
-    {content: "Pikachu", correct_answer: false},
-    {content: "Rick Astley", correct_answer: false}])
-  },
-  {content: pikachu[:description], quizz: quizz1, answers: Answer.create([
-    {content: "General Kenobi", correct_answer: false},
-    {content: "Bulbasaur", correct_answer: false},
-    {content: "Charmander", correct_answer: false},
-    {content: "Pikachu", correct_answer: true}])
-  },
-  {content: eevee[:description], quizz: quizz1, answers: Answer.create([
-    {content: "General Grievous", correct_answer: false},
-    {content: "Eevee", correct_answer: true},
-    {content: "Pikachu", correct_answer: false},
-    {content: "Squirtle", correct_answer: false}])
-  },
-  {content: squirtle[:description], quizz: quizz1, answers: Answer.create([
-    {content: "Charizard", correct_answer: false},
-    {content: "Charmander", correct_answer: false},
-    {content: "Squirtle", correct_answer: true},
-    {content: "Psykokwak", correct_answer: false}])
-  },
-  {content: charmander[:description], quizz: quizz1, answers: Answer.create([
-    {content: "Charizard", correct_answer: false},
-    {content: "Charmander", correct_answer: true},
-    {content: "Agumon", correct_answer: false},
-    {content: "Muk", correct_answer: false}])
-  },
-])
 
 quizz1.save
 p "quizz 1 created"
 
 quizz2 = Quizz.create(
-  title: "Pokemon habitats"
+  title: "Pokemon habitats",
+  questions: Question.create([
+    {content: "Bulbasaur usualy lives in #{bulbasaur[:habitat]}", quizz: quizz2, answers: Answer.create([
+      {content: "True", correct_answer: true},
+      {content: "False", correct_answer: false}])
+    },
+    {content: "Pikachu usualy lives in #{eevee[:habitat]}", quizz: quizz2, answers: Answer.create([
+      {content: "True", correct_answer: false},
+      {content: "False", correct_answer: true}])
+    },
+    {content: "Squirtle usualy lives in #{bulbasaur[:habitat]}", quizz: quizz2, answers: Answer.create([
+      {content: "True", correct_answer: false},
+      {content: "False", correct_answer: true}])
+    },
+    {content: "Charmander usualy lives in #{charmander[:habitat]}", quizz: quizz2, answers: Answer.create([
+      {content: "True", correct_answer: true},
+      {content: "False", correct_answer: false}])
+    },
+    {content: "Eevee usualy lives in #{eevee[:habitat]}", quizz: quizz2, answers: Answer.create([
+      {content: "True", correct_answer: true},
+      {content: "False", correct_answer: false}])
+    },
+  ])
 )
-
-questions_quizz_2 = Question.create([
-  {content: "Bulbasaur usualy lives in #{bulbasaur[:habitat]}", quizz: quizz2, answers: Answer.create([
-    {content: "True", correct_answer: true},
-    {content: "False", correct_answer: false}])
-  },
-  {content: "Pikachu usualy lives in #{eevee[:habitat]}", quizz: quizz2, answers: Answer.create([
-    {content: "True", correct_answer: false},
-    {content: "False", correct_answer: true}])
-  },
-  {content: "Squirtle usualy lives in #{bulbasaur[:habitat]}", quizz: quizz2, answers: Answer.create([
-    {content: "True", correct_answer: false},
-    {content: "False", correct_answer: true}])
-  },
-  {content: "Charmander usualy lives in #{charmander[:habitat]}", quizz: quizz2, answers: Answer.create([
-    {content: "True", correct_answer: true},
-    {content: "False", correct_answer: false}])
-  },
-  {content: "Eevee usualy lives in #{eevee[:habitat]}", quizz: quizz2, answers: Answer.create([
-    {content: "True", correct_answer: true},
-    {content: "False", correct_answer: false}])
-  },
-])
 
 quizz2.save
 p "quizz 2 created"
 
 quizz3 = Quizz.create(
-  title: "Pokemon types"
-)
-
-questions_quizz_3 = Question.create([
-  {content: "What's one of Bulbasaur's pokemon types?", quizz: quizz3, answers: Answer.create([
-    {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
-    {content: "#{pikachu[:types][0].type.name}", correct_answer: false},
-    {content: "#{eevee[:types][0].type.name}", correct_answer: false}])
-  },
-  {content: "What's one of Pikachu's pokemon types?", quizz: quizz3, answers: Answer.create([
-    {content: "#{charmander[:types][0].type.name}", correct_answer: false},
-    {content: "#{pikachu[:types][0].type.name}", correct_answer: true},
-    {content: "#{squirtle[:types][0].type.name}", correct_answer: false}])
-  },
-  {content: "What's one of Squirtle's pokemon types?", quizz: quizz3, answers: Answer.create([
-    {content: "#{bulbasaur[:types][0].type.name}", correct_answer: false},
-    {content: "#{eevee[:types][0].type.name}", correct_answer: true},
-    {content: "#{squirtle[:types][0].type.name}", correct_answer: true}])
-  },
-  {content: "What's one of Eevee's pokemon types?", quizz: quizz3, answers: Answer.create([
-    {content: "#{charmander[:types][0].type.name}", correct_answer: false},
-    {content: "#{pikachu[:types][0].type.name}", correct_answer: false},
-    {content: "#{eevee[:types][0].type.name}", correct_answer: true}])
-  },
-  {content: "What's one of Charmander's pokemon types?", quizz: quizz3, answers: Answer.create([
-    {content: "#{squirtle[:types][0].type.name}", correct_answer: false},
-    {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
-    {content: "#{charmander[:types][0].type.name}", correct_answer: false}])
-  },
-])
+  title: "Pokemon types",
+  questions: Question.create([
+    {content: "What's one of Bulbasaur's pokemon types?", quizz: quizz3, answers: Answer.create([
+      {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
+      {content: "#{pikachu[:types][0].type.name}", correct_answer: false},
+      {content: "#{eevee[:types][0].type.name}", correct_answer: false}])
+    },
+    {content: "What's one of Pikachu's pokemon types?", quizz: quizz3, answers: Answer.create([
+      {content: "#{charmander[:types][0].type.name}", correct_answer: false},
+      {content: "#{pikachu[:types][0].type.name}", correct_answer: true},
+      {content: "#{squirtle[:types][0].type.name}", correct_answer: false}])
+    },
+    {content: "What's one of Squirtle's pokemon types?", quizz: quizz3, answers: Answer.create([
+      {content: "#{bulbasaur[:types][0].type.name}", correct_answer: false},
+      {content: "#{eevee[:types][0].type.name}", correct_answer: true},
+      {content: "#{squirtle[:types][0].type.name}", correct_answer: true}])
+    },
+    {content: "What's one of Eevee's pokemon types?", quizz: quizz3, answers: Answer.create([
+      {content: "#{charmander[:types][0].type.name}", correct_answer: false},
+      {content: "#{pikachu[:types][0].type.name}", correct_answer: false},
+      {content: "#{eevee[:types][0].type.name}", correct_answer: true}])
+    },
+    {content: "What's one of Charmander's pokemon types?", quizz: quizz3, answers: Answer.create([
+      {content: "#{squirtle[:types][0].type.name}", correct_answer: false},
+      {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
+      {content: "#{charmander[:types][0].type.name}", correct_answer: false}])
+    },
+  ])
+) 
 
 quizz3.save
 p "quizz 3 created"
 
 quizz4 = Quizz.create(
-  title: "Mixed quizz"
+  title: "Mixed quizz",
+  questions: Question.create([
+    {content: "What's one of Pikachu's pokemon types?", quizz: quizz4, answers: Answer.create([
+      {content: "#{charmander[:types][0].type.name}", correct_answer: false},
+      {content: "#{pikachu[:types][0].type.name}", correct_answer: true},
+      {content: "#{squirtle[:types][0].type.name}", correct_answer: false}])
+    },
+    {content: "Eevee usualy lives in #{charmander[:habitat]}", quizz: quizz4, answers: Answer.create([
+      {content: "True", correct_answer: true},
+      {content: "False", correct_answer: false}])
+    },
+    {content: charmander[:description], quizz: quizz4, answers: Answer.create([
+      {content: "Charizard", correct_answer: false},
+      {content: "Charmander", correct_answer: true},
+      {content: "Agumon", correct_answer: false},
+      {content: "Muk", correct_answer: false}])
+    },
+    {content: "Pikachu usualy lives in #{eevee[:habitat]}", quizz: quizz4, answers: Answer.create([
+      {content: "True", correct_answer: false},
+      {content: "False", correct_answer: true}])
+    },
+    {content: "What's one of Charmander's pokemon types?", quizz: quizz4, answers: Answer.create([
+      {content: "#{squirtle[:types][0].type.name}", correct_answer: false},
+      {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
+      {content: "#{charmander[:types][0].type.name}", correct_answer: false}])
+    },
+  ])
 )
-
-questions_quizz_4 = Question.create([
-  {content: "What's one of Pikachu's pokemon types?", quizz: quizz4, answers: Answer.create([
-    {content: "#{charmander[:types][0].type.name}", correct_answer: false},
-    {content: "#{pikachu[:types][0].type.name}", correct_answer: true},
-    {content: "#{squirtle[:types][0].type.name}", correct_answer: false}])
-  },
-  {content: "Eevee usualy lives in #{charmander[:habitat]}", quizz: quizz4, answers: Answer.create([
-    {content: "True", correct_answer: true},
-    {content: "False", correct_answer: false}])
-  },
-  {content: charmander[:description], quizz: quizz4, answers: Answer.create([
-    {content: "Charizard", correct_answer: false},
-    {content: "Charmander", correct_answer: true},
-    {content: "Agumon", correct_answer: false},
-    {content: "Muk", correct_answer: false}])
-  },
-  {content: "Pikachu usualy lives in #{eevee[:habitat]}", quizz: quizz4, answers: Answer.create([
-    {content: "True", correct_answer: false},
-    {content: "False", correct_answer: true}])
-  },
-  {content: "What's one of Charmander's pokemon types?", quizz: quizz4, answers: Answer.create([
-    {content: "#{squirtle[:types][0].type.name}", correct_answer: false},
-    {content: "#{bulbasaur[:types][0].type.name}", correct_answer: true},
-    {content: "#{charmander[:types][0].type.name}", correct_answer: false}])
-  },
-])
 
 quizz4.save
 p "quizz 4 created"
--- test/models/quizz_test.rb
+++ test/models/quizz_test.rb
@@ -7,37 +7,65 @@ class QuizzTest < ActiveSupport::TestCase
   end
 
   test "Should have a maximum of five questions" do
-    quizz2 = Quizz.create(
+    quizz = Quizz.new(
       title: "test"
     )
-    
-    questions_quizz_2 = Question.create([
-      {content: "Bulbasaur usualy lives in grassland", quizz: quizz2, answers: Answer.create([
+    questions_quizz = Question.create([
+      {content: "Bulbasaur usualy lives in grassland", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: true},
         {content: "False", correct_answer: false}])
       },
-      {content: "Pikachu usualy lives in america", quizz: quizz2, answers: Answer.create([
+      {content: "Pikachu usualy lives in america", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: false},
         {content: "False", correct_answer: true}])
       },
-      {content: "Squirtle usualy lives in mountain", quizz: quizz2, answers: Answer.create([
+      {content: "Squirtle usualy lives in mountain", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: false},
         {content: "False", correct_answer: true}])
       },
-      {content: "Charmander usualy lives in borneo", quizz: quizz2, answers: Answer.create([
+      {content: "Charmander usualy lives in borneo", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: true},
         {content: "False", correct_answer: false}])
       },
-      {content: "Eevee usualy lives in paris", quizz: quizz2, answers: Answer.create([
+      {content: "Eevee usualy lives in paris", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: true},
         {content: "False", correct_answer: false}])
       },
-      {content: "Psykokwak usualy lives in mexico", quizz: quizz2, answers: Answer.create([
+      {content: "Psykokwak usualy lives in mexico", quizz: quizz, answers: Answer.create([
         {content: "True", correct_answer: true},
         {content: "False", correct_answer: false}])
       }
     ])
-    
-    assert_not quizz2.save
+    assert_not quizz.save
   end
+
+  test "Should save quizz with proper parameters" do
+    quizz = Quizz.create(
+      title: "test",
+      questions: Question.create([
+        {content: "Bulbasaur usualy lives in grassland", quizz: quizz, answers: Answer.create([
+          {content: "True", correct_answer: true},
+          {content: "False", correct_answer: false}])
+        },
+        {content: "Pikachu usualy lives in america", quizz: quizz, answers: Answer.create([
+          {content: "True", correct_answer: false},
+          {content: "False", correct_answer: true}])
+        },
+        {content: "Squirtle usualy lives in mountain", quizz: quizz, answers: Answer.create([
+          {content: "True", correct_answer: false},
+          {content: "False", correct_answer: true}])
+        },
+        {content: "Charmander usualy lives in borneo", quizz: quizz, answers: Answer.create([
+          {content: "True", correct_answer: true},
+          {content: "False", correct_answer: false}])
+        },
+        {content: "Eevee usualy lives in paris", quizz: quizz, answers: Answer.create([
+          {content: "True", correct_answer: true},
+          {content: "False", correct_answer: false}])
+        }
+      ])
+    )
+    assert quizz.save
+  end
+  
 end
